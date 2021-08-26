942d27335085964c2a90095f4783e2cd39afe7a9
hermionewarr <hermionewarr@icloud.com>
2021-08-26 13:26:25 +0100

    mrofsnart

--- locating_c3/test_neckNavigator.py
+++ locating_c3/test_neckNavigator.py
@@ -13,15 +13,42 @@ import torch
 from neckNavigatorUtils import k_fold_split_train_val_test
 import pandas as pd
 
+def mrofsnart(net_slice, transforms, shape = 128, coords = None, test_inds = None):#transforms backwards
+    #might have get transform indices for test data
+    if test_inds is not None:
+        transforms = [transforms[ind] for ind in test_inds]
+    x_arr,y_arr,z_arr = [],[],[]
+    for i in range(len(net_slice)):
+        #undo scale
+        net_slice *= 14/16
+        #undo crop
+        #eg z crop [46,1] z=12  [[true, crop array]<- crop[zmin, zmax, xmin,...],]
+        z = net_slice + transforms[i,1][0]
+        if coords != None:
+            x = coords[0]*2
+            y = coords[1]*2
+            x += transforms[i,1][2]
+            y += transforms[i,1][4]
+            x_arr.append(x)
+        #undo flip if necessary
+        if (transforms[i,0]==True):
+            z = shape - z
+            z_arr.append(z)
+            if coords != None:
+                y = shape - y
+                y_arr.append(y)
+    return x_arr,y_arr,z_arr
+
 def main():
 
     # get data
     #data_path =  '/home/olivia/Documents/Internship_sarcopenia/locating_c3/preprocessed_sphere.npz'
-    data_path = '/home/hermione/Documents/Internship_sarcopenia/locating_c3/preprocessed_gauss2.npz'
+    data_path = '/home/hermione/Documents/Internship_sarcopenia/locating_c3/preprocessed_Tgauss2.npz'
     data = get_data(data_path)
     inputs = data[0]
     targets = data[1]
     ids = data[2]
+    transforms = data[3]
 
     val_workers = int(4)
 
@@ -29,18 +56,16 @@ def main():
     test_dataset = neckNavigatorDataset(inputs = inputs, targets = targets, image_inds = test_inds)
     test_dataloader = DataLoader(dataset= test_dataset, batch_size = 1, shuffle=False, pin_memory=True, num_workers=val_workers, worker_init_fn=lambda _: np.random.seed(int(torch.initial_seed())%(2**32-1)))
     print(test_inds)
-    model_dir = "/home/olivia/Documents/Internship_sarcopenia/locating_c3/attempt1/"
+    #model_dir = "/home/olivia/Documents/Internship_sarcopenia/locating_c3/attempt1/"
     #testdataloader_dir = "/home/olivia/Documents/Internship_sarcopenia/locating_c3/attempt1/test_dataloader.pt"
     device = 'cuda:0'
 
     #tester = neckNavigatorTest2(model_dir, test_dataloader, device)
-    #checkpoint_dir = checkpoint_dir = "/home/hermione/Documents/Internship_sarcopenia/locating_c3/model_ouputs"
+    model_dir =  "/home/hermione/Documents/Internship_sarcopenia/locating_c3/model_ouputs"
     model = neckNavigator()
     #tester = neckNavigatorTest1(model, checkpoint_dir, test_dataloader, device)
 
     tester = neckNavigatorTest2(model_dir, test_dataloader, device)
-    #tester = neckNavigatorTest1(model, test_dataloader, device)
-
 
     #test_results = tester
     C3s, segments, GTs = tester
